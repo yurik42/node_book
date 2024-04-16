c10de54e4ad443fe7321acfd350815b1510f25fe
Yury Kuznetsov <yurik@bluemarblegeo.com>
2024-04-16 07:47:08 -0400

    Update cesium_tile_transform/transform.ts

--- unsorted/cesium_tile_transform/transform.ts
+++ unsorted/cesium_tile_transform/transform.ts
@@ -24,7 +24,34 @@ const cartesian = Cartographic.toCartesian(cartographic);
 const enuMatrix = Transforms.eastNorthUpToFixedFrame(cartesian);
 const transform = Matrix4.toArray(enuMatrix);
 
-const text = JSON.stringify(transform, null, 2);
+{
+    const text = JSON.stringify(transform, null, 2);
+    console.log(text);
+}
+{
+    const text = JSON.stringify(cartographic, null, 2);
+    console.log("cartographic: %s", text);
+}
 
-console.log(text);
+var root = {
+    geo_location_degrees: [
+        lonDegrees,
+        latDegrees,
+        height
+    ],
+    cartographic: cartographic,
+    cartesian: cartesian,
+    enuMatrix: enuMatrix,
+    transform: transform
+}
+
+{
+    const text = JSON.stringify(root, null, 2);
+    console.log("root: %s", text);
+
+    if (!fs.existsSync("out"))
+        fs.mkdirSync("out");
+    fs.writeFileSync("out/root.json", text);
+}
+
 console.log('Done...');
