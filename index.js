7645679435b0b3ca4d4e56f87fd47bed22e452aa
Yuri Y. Kuznetsov <yyk@kestrel>
2024-04-16 05:10:51 -0400

    Update to page 13

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.url)
-    res.end('Hello Node.js')
+    if (req.url === '/about')
+        res.end('The about page')
+    else if (req.url === '/contact')
+        res.end('The contact page')
+    else if (req.url === '/')
+        res.end('The home page')
+    else {
+        res.writeHead(404)
+        res.end('page not found')
+    }
+
+    // Lim, Greg. Beginning Node.js, Express & MongoDB Development (p. 13). Kindle Edition. 
 })
 
 server.listen(3000)
